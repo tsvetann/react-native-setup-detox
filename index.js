77f79ee116e98116a43b4d35a27600135dc1f174
Tsvetan Nikolov <tsvetan.nikolov@coliquio.de>
2020-10-29 18:57:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+"use strict";
+
+const clear = require("clear");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const { updateFiles, installPackages, detoxInit, nextSteps } = require("./lib");
+const reactNativeConfig = require("./lib/reactNativeConfig");
+
+clear();
+
+console.log(
+  chalk.green(figlet.textSync("Setting up Detox", { horizontalLayout: "full" }))
+);
+
+async function init() {
+  const config = await reactNativeConfig();
+  installPackages();
+  detoxInit(config.project.android);
+  await updateFiles(config.project.android);
+  nextSteps();
+}
+
+init()
+  .then(() => console.log(chalk.green("Installation complete")))
+  .catch(console.error);
